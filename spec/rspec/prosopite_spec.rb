098c245eff651b5056931bb137d2aa0a5e1d3875
Hiroshi Kajisha <kajisha@gmail.com>
2022-09-13 16:05:22 +0900

    bundle gem .

---
+++ spec/rspec/prosopite_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rspec::Prosopite do
+  it "has a version number" do
+    expect(Rspec::Prosopite::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
